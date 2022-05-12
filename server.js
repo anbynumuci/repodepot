dfd6a9d6f42a7e00fa30f17876ed5af1d5e4b027
Lexsi Bynum <103914962+anbynumuci@users.noreply.github.com>
2022-05-12 11:39:17 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+//set up static file server
+let static = require('node-static');
+
+//http server
+let http = require('http');
+
+//assume heroku
+let port = process.env.PORT;
+let directory = __dirname + '/public';
+
+if ((typeof port == 'undefined') || (port == null)){
+	port = 8080;
+	directory = '/public'
+}
+
+let file = new static.Server(directory);
+
+
+let app = http.createServer(
+	function(request, response){
+		request.addListener('end',
+			function(){
+				file.serve(request,response)
+			}).resume();
+	}).listen(port);
+
+console.log('the server is running');
